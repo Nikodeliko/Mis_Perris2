bc91d25a5458ae3b9b5b67a1fb69af2041396a5b
Nikodeliko <44078433+Nikodeliko@users.noreply.github.com>
2018-10-26 10:27:22 -0300

    Agregue mi App
    Agregue mi app al archivo settings.py

--- BasedeDatos/BasedeDatos/settings.py
+++ BasedeDatos/BasedeDatos/settings.py
@@ -37,8 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-	'BasedeDatos.Apps.Servidor'
-	
+	'BasedeDatos.Apps.Servidor',
+    'BasedeDatos.Apps.MantenedorPerros'
 ]
 
 MIDDLEWARE = [
